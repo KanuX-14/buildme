99585e766f894e7b77b176d1d079d8e392659ab3
KanuX-14 <kanux.dev@gmail.com>
2023-05-26 08:01:34 -0300

    Added root check
    It will also check which root manager is used.
    None will result in using "su -c", as the user will need to provide password for every command.

--- buildme.sh
+++ buildme.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ROOT=""
 OLD_VERSION=$(uname -r)
 VERSION=$(make -s kernelversion)
 CORE=1 # $(nproc)
@@ -14,6 +15,31 @@ BLUE="\x1b[1;34m"
 PURPLE="\x1b[1;35m"
 CYAN="\x1b[1;36m"
 RESET_COLOUR="\x1b[0m"
+
+# int: Checks if script is running with root.
+#      If not, no updates will happen.
+#      The script will compile but will not install.
+AM_I_ROOT()
+{
+  local user=$(whoami)
+
+  case $user in
+  root) return 0 ;;
+  *) return 1 ;;
+  esac
+}
+
+# void: Get the root manager name.
+#       If no root manager is found, "sh -c" will be used.
+#       Therefore the user will need to insert the password for every
+#         root dependable command.
+GET_ROOT_MANAGER()
+{
+  if type sudo &>/dev/null; then ROOT="sudo"
+  elif type doas &>/dev/null; then ROOT="doas"
+  else ROOT="su -c"
+  fi
+}
 
 MAKE_PACKAGES()
 {
@@ -34,26 +60,34 @@ MAKE_PACKAGES()
 # Mostly used in Debian-based distributions
 AUTOMATED_INSTALL()
 {
-  sudo make modules_install
-  sudo make headers_install
-  sudo make install
-  sudo update-initramfs -u
+  ${ROOT} make modules_install
+  ${ROOT} make headers_install
+  ${ROOT} make install
+  ${ROOT} update-initramfs -u
 }
 
 MANUAL_INSTALL()
 {
-  sudo make modules_install
-  sudo make headers_install
+  ${ROOT} make modules_install
+  ${ROOT} make headers_install
   VERSION=$(ls /lib/modules | grep "${VERSION}" | sort --reverse - | head -n 1)
-  sudo cp -v arch/x86/boot/bzImage /boot/vmlinuz-linux-${VERSION}
-  sudo mkinitcpio -k ${VERSION} -g /boot/initramfs-linux-${VERSION}.img
-  sudo cp -v System.map /boot/System.map-${VERSION}
-  sudo ln -sfv /boot/System.map-${VERSION} /boot/System.map
-  sudo cp -v .config /boot/config-${VERSION}
+  ${ROOT} cp -v arch/x86/boot/bzImage /boot/vmlinuz-linux-${VERSION}
+  ${ROOT} mkinitcpio -k ${VERSION} -g /boot/initramfs-linux-${VERSION}.img
+  ${ROOT} cp -v System.map /boot/System.map-${VERSION}
+  ${ROOT} ln -sfv /boot/System.map-${VERSION} /boot/System.map
+  ${ROOT} cp -v .config /boot/config-${VERSION}
 }
 
+# Check about root
+if ! AM_I_ROOT
+  then
+    printf "${YELLOW}""Warning: Script running without root privileges. If no recognized root manager is found, the script will use \"su -c\" instead...""${RESET_COLOUR}""\n"
+    GET_ROOT_MANAGER
+fi
+
+
 # Making sure you own every file in the kernel
-sudo chown -R ${CURRENT_USER}:${CURRENT_USER} ./*
+${ROOT} chown -R ${CURRENT_USER}:${CURRENT_USER} ./*
 
 # Start building
 MAKE_PACKAGES
@@ -68,9 +102,9 @@ fi
 read -p "Remove the ${OLD_VERSION} kernel? [N/y] " REMOVE_KERNEL
 case ${REMOVE_KERNEL} in
   "Y"|"y"|"Yes"|"yes")
-    sudo rm -fv /boot/*${OLD_VERSION}*
-    sudo rm -rfv /lib/modules/*${OLD_VERSION}*
-    sudo rm -fv /etc/mkinitcpio.d/*${OLD_VERSION}*
+    ${ROOT} rm -fv /boot/*${OLD_VERSION}*
+    ${ROOT} rm -rfv /lib/modules/*${OLD_VERSION}*
+    ${ROOT} rm -fv /etc/mkinitcpio.d/*${OLD_VERSION}*
     printf "${YELLOW}""Warning: ${OLD_VERSION} kernel has been removed.""${RESET_COLOUR}""\n"
     ;;
   *)
@@ -81,8 +115,8 @@ printf "\n"
 
 # Finish installation and prompt reboot
 if type dpkg &>/dev/null
-  then sudo update-grub
-  else sudo grub-mkconfig -o /boot/grub/grub.cfg
+  then ${ROOT} update-grub
+  else ${ROOT} grub-mkconfig -o /boot/grub/grub.cfg
 fi
 
 printf "Reboot in order to apply the changes.\n"
